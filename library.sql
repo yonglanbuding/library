15a1f22d53237912ba0cdac5565da879e48aa70b
yonglanbuding <153484521+yonglanbuding@users.noreply.github.com>
2023-12-11 15:39:08 +0800

    Add files via upload

--- library.sql
+++ library.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : agan
  Source Server Type    : MySQL
- Source Server Version : 50730
+ Source Server Version : 80032
  Source Host           : localhost:3306
  Source Schema         : library
 
  Target Server Type    : MySQL
- Target Server Version : 50730
+ Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 25/01/2021 10:55:46
+ Date: 11/12/2023 13:28:12
 */
 
 SET NAMES utf8mb4;
@@ -22,26 +22,25 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `book_info`;
 CREATE TABLE `book_info`  (
-  `book_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '图书号',
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '书名',
-  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '作者',
-  `publish` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '出版社',
-  `ISBN` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'ISBN',
-  `introduction` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '简介',
-  `language` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '语言',
+  `book_id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '图书号',
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '书名',
+  `author` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '作者',
+  `publish` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '出版社',
+  `ISBN` varchar(13) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'ISBN',
+  `introduction` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL COMMENT '简介',
+  `language` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '语言',
   `price` decimal(10, 2) NOT NULL COMMENT '价格',
   `pubdate` date NULL DEFAULT NULL COMMENT '出版日期',
-  `class_id` int(11) NULL DEFAULT NULL COMMENT '分类号',
-  `pressmark` int(11) NULL DEFAULT NULL COMMENT '书架号',
-  `state` smallint(6) NULL DEFAULT NULL COMMENT '状态',
-  PRIMARY KEY (`book_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 50000012 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `class_id` int(0) NULL DEFAULT NULL COMMENT '分类号',
+  `pressmark` int(0) NULL DEFAULT NULL COMMENT '书架号',
+  `state` smallint(0) NULL DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`book_id`, `name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 50000013 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of book_info
 -- ----------------------------
-INSERT INTO `book_info` VALUES (10000001, '大雪中的山庄', '东野圭吾', '北京十月文艺出版社', '9787530216835', '东野圭吾长篇小说杰作，中文简体首次出版。 一出没有剧本的舞台剧，为什么能让七个演员赌上全部人生.东野圭吾就是有这样过人的本领，能从充满悬念的案子写出荡气回肠的情感，在极其周密曲折的同时写出人性的黑暗与美丽。 一家与外界隔绝的民宿里，七个演员被要求住满四天，接受导演的考验，但不断有人失踪。难道这并非正常排练，而是有人布下陷阱要杀他们。 那时候我开始喜欢上戏剧和音乐，《大雪中的山庄》一书的灵感就来源于此。我相信这次的诡计肯定会让人大吃一惊。', '中文', 35.00, '2017-07-23', 9, 13, 1);
-INSERT INTO `book_info` VALUES (10000003, '三生三世 十里桃花', '唐七公子 ', '沈阳出版社', '9787544138000', '三生三世，她和他，是否注定背负一段纠缠的姻缘？\r\n三生三世，她和他，是否终能互许一个生生世世的承诺？', '中文', 26.80, '2009-01-06', 7, 2, 1);
+INSERT INTO `book_info` VALUES (10000003, '三生三世 十里桃花', '唐七公子 ', '沈阳出版社', '9787544138000', '三生三世，她和他，是否注定背负一段纠缠的姻缘？\r\n三生三世，她和他，是否终能互许一个生生世世的承诺？', '中文', 26.80, '2009-01-06', 7, 2, 0);
 INSERT INTO `book_info` VALUES (10000004, '何以笙箫默', '顾漫 ', '朝华出版社', '9787505414709', '一段年少时的爱恋，牵出一生的纠缠。大学时代的赵默笙阳光灿烂，对法学系大才子何以琛一见倾心，开朗直率的她拔足倒追，终于使才气出众的他为她停留驻足。然而，不善表达的他终于使她在一次伤心之下远走他乡……', '中文', 15.00, '2007-04-03', 7, 2, 1);
 INSERT INTO `book_info` VALUES (10000005, '11处特工皇妃', '潇湘冬儿', '江苏文艺出版社', '9787539943893', '《11处特工皇妃(套装上中下册)》内容简介：她是国安局军情十一处惊才绝艳的王牌军师——收集情报、策划部署、进不友好国家布置暗杀任务……她运筹帷幄之中，决胜于千里之外，堪称军情局大厦的定海神针。', '中文', 74.80, '2011-05-05', 7, 2, 1);
 INSERT INTO `book_info` VALUES (10000006, '人类简史', '[以色列] 尤瓦尔·赫拉利 ', '中信出版社', '9787508647357', '十万年前，地球上至少有六种不同的人\r\n但今日，世界舞台为什么只剩下了我们自己？\r\n从只能啃食虎狼吃剩的残骨的猿人，到跃居食物链顶端的智人，\r\n从雪维洞穴壁上的原始人手印，到阿姆斯壮踩上月球的脚印，\r\n从认知革命、农业革命，到科学革命、生物科技革命，\r\n我们如何登上世界舞台成为万物之灵的？\r\n从公元前1776年的《汉摩拉比法典》，到1776年的美国独立宣言，\r\n从帝国主义、资本主义，到自由主义、消费主义，\r\n从兽欲，到物欲，从兽性、人性，到神性，\r\n我们了解自己吗？我们过得更快乐吗？\r\n我们究竟希望自己得到什么、变成什么？', '英文', 68.00, '2014-11-01', 11, 3, 1);
@@ -53,17 +52,18 @@ INSERT INTO `book_info` VALUES (50000007, '造彩虹的人', '东野圭吾 ', '�
 INSERT INTO `book_info` VALUES (50000008, '控方证人', '阿加莎·克里斯蒂 ', '新星出版社', '9787513325745', '经典同名话剧六十年常演不衰； 比利•怀尔德执导同名电影，获奥斯卡金像奖六项提名！ 阿加莎对神秘事物的向往大约来自于一种女性祖先的遗传，在足不出户的生活里，生出对世界又好奇又恐惧的幻想。 ——王安忆 伦纳德•沃尔被控谋杀富婆艾米丽以图染指其巨额遗产，他却一再表明自己的无辜。伦纳德的妻子是唯一能够证明他无罪的证人，却以控方证人的身份出庭指证其确实犯有谋杀罪。伦纳德几乎陷入绝境，直到一个神秘女人的出现…… 墙上的犬形图案；召唤死亡的收音机；蓝色瓷罐的秘密；一只疯狂的灰猫……十一篇神秘的怪谈，最可怕的不是“幽灵”，而是你心中的魔鬼。', '中文', 35.00, '2017-05-01', 9, 12, 1);
 INSERT INTO `book_info` VALUES (50000009, '少有人走的路', 'M·斯科特·派克 ', '吉林文史出版社', '9787807023777', '这本书处处透露出沟通与理解的意味，它跨越时代限制，帮助我们探索爱的本质，引导我们过上崭新，宁静而丰富的生活；它帮助我们学习爱，也学习独立；它教诲我们成为更称职的、更有理解心的父母。归根到底，它告诉我们怎样找到真正的自我。 正如开篇所言：人生苦难重重。M·斯科特·派克让我们更加清楚：人生是一场艰辛之旅，心智成熟的旅程相当漫长。但是，他没有让我们感到恐惧，相反，他带领我们去经历一系列艰难乃至痛苦的转变，最终达到自我认知的更高境界。', '中文', 26.00, '2007-01-01', 9, 12, 1);
 INSERT INTO `book_info` VALUES (50000010, '追寻生命的意义', '[奥] 维克多·弗兰克 ', '新华出版社', '9787501162734', '《追寻生命的意义》是一个人面对巨大的苦难时，用来拯救自己的内在世界，同时也是一个关于每个人存在的价值和能者多劳们生存的社会所应担负职责的思考。本书对于每一个想要了解我们这个时代的人来说，都是一部必不可少的读物。这是一部令人鼓舞的杰作……他是一个不可思议的人，任何人都可以从他无比痛苦的经历中，获得拯救自己的经验……高度推荐。', '中文', 12.00, '2003-01-01', 9, 16, 0);
-INSERT INTO `book_info` VALUES (50000011, '秘密花园', '乔汉娜·贝斯福 ', '北京联合出版公司', '9787550252585', '欢迎来到秘密花园！ 在这个笔墨编织出的美丽世界中漫步吧 涂上你喜爱的颜色，为花园带来生机和活力 发现隐藏其中的各类小生物，与它们共舞 激活创造力，描绘那些未完成的仙踪秘境 各个年龄段的艺术家和“园丁”都可以来尝试喔！', '中文', 42.00, '2015-06-01', 9, 18, 1);
+INSERT INTO `book_info` VALUES (50000011, '秘密花园', '乔汉娜·贝斯福 ', '北京联合出版公司', '9787550252585', '欢迎来到秘密花园！ 在这个笔墨编织出的美丽世界中漫步吧 涂上你喜爱的颜色，为花园带来生机和活力 发现隐藏其中的各类小生物，与它们共舞 激活创造力，描绘那些未完成的仙踪秘境 各个年龄段的艺术家和“园丁”都可以来尝试喔！', '中文', 42.00, '2015-06-01', 9, 18, 0);
+INSERT INTO `book_info` VALUES (50000012, '秋刀鱼', '甘', '广应科', '202110005200', '牛逼', '中文', 30.00, '2012-01-12', 1, 12, 12);
 
 -- ----------------------------
 -- Table structure for class_info
 -- ----------------------------
 DROP TABLE IF EXISTS `class_info`;
 CREATE TABLE `class_info`  (
-  `class_id` int(11) NOT NULL COMMENT '分类号',
-  `class_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类名',
+  `class_id` int(0) NOT NULL COMMENT '分类号',
+  `class_name` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '分类名',
   PRIMARY KEY (`class_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of class_info
@@ -96,38 +96,42 @@ INSERT INTO `class_info` VALUES (22, '综合');
 -- ----------------------------
 DROP TABLE IF EXISTS `lend_list`;
 CREATE TABLE `lend_list`  (
-  `sernum` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '流水号',
-  `book_id` bigint(20) NOT NULL COMMENT '图书号',
-  `reader_id` int(11) NOT NULL COMMENT '读者卡号',
+  `sernum` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '流水号',
+  `book_id` bigint(0) NOT NULL COMMENT '图书号',
+  `reader_id` int(0) NOT NULL COMMENT '读者卡号',
   `lend_date` date NULL DEFAULT NULL COMMENT '借出日期',
   `back_date` date NULL DEFAULT NULL COMMENT '归还日期',
   PRIMARY KEY (`sernum`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2017090202 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2017090202 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of lend_list
 -- ----------------------------
-INSERT INTO `lend_list` VALUES (2017031501, 10000001, 1706010217, '2017-03-15', '2017-06-16');
-INSERT INTO `lend_list` VALUES (2017031502, 50000004, 1706010217, '2017-03-15', '2017-09-03');
-INSERT INTO `lend_list` VALUES (2017061001, 10000003, 1706010218, '2017-06-10', '2017-09-02');
-INSERT INTO `lend_list` VALUES (2017061201, 10000006, 1706010219, '2017-06-12', '2017-09-02');
-INSERT INTO `lend_list` VALUES (2017061501, 50000005, 1706010219, '2017-06-15', NULL);
-INSERT INTO `lend_list` VALUES (2017061502, 50000010, 1706010218, '2017-06-15', NULL);
-INSERT INTO `lend_list` VALUES (2017090201, 10000001, 1706010220, '2017-09-02', '2017-09-02');
+INSERT INTO `lend_list` VALUES (1, 50000012, 170601000, '2023-12-11', '2023-12-11');
+INSERT INTO `lend_list` VALUES (2, 10000001, 1706010217, '2017-03-15', '2017-06-16');
+INSERT INTO `lend_list` VALUES (3, 50000004, 1706010217, '2017-03-15', '2017-09-03');
+INSERT INTO `lend_list` VALUES (4, 10000003, 1706010218, '2017-06-10', '2017-09-02');
+INSERT INTO `lend_list` VALUES (5, 10000006, 1706010219, '2017-06-12', '2017-09-02');
+INSERT INTO `lend_list` VALUES (6, 50000005, 1706010219, '2017-06-15', NULL);
+INSERT INTO `lend_list` VALUES (7, 50000010, 1706010218, '2017-06-15', NULL);
+INSERT INTO `lend_list` VALUES (8, 10000001, 1706010220, '2017-09-02', '2017-09-02');
+INSERT INTO `lend_list` VALUES (9, 50000011, 170601000, '2023-12-12', '2023-12-12');
+INSERT INTO `lend_list` VALUES (2017090203, 50000012, 170601000, '2023-12-11', '2023-12-11');
+INSERT INTO `lend_list` VALUES (2017090204, 50000012, 170601000, '2023-12-11', '2023-12-11');
+INSERT INTO `lend_list` VALUES (2017090205, 50000012, 170601000, '2023-12-12', '2023-12-11');
 
 -- ----------------------------
 -- Table structure for reader_info
 -- ----------------------------
 DROP TABLE IF EXISTS `reader_info`;
 CREATE TABLE `reader_info`  (
-  `reader_id` int(11) NOT NULL COMMENT '卡号',
-  `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
-  `sex` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
+  `reader_id` int(0) NOT NULL COMMENT '卡号',
+  `name` varchar(16) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '姓名',
+  `sex` varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '性别',
   `birth` date NULL DEFAULT NULL COMMENT '生日',
-  `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
-  `telcode` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '电话',
-  PRIMARY KEY (`reader_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `address` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '地址',
+  `telcode` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '电话'
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of reader_info
@@ -138,27 +142,44 @@ INSERT INTO `reader_info` VALUES (1706010219, '李四', '男', '1999-01-01', '�
 INSERT INTO `reader_info` VALUES (1706010220, '小华', '男', '1999-08-29', '陕西省西安市', '12345678907');
 INSERT INTO `reader_info` VALUES (1706010221, '小红', '女', '1999-01-01', '陕西省西安市', '15123659875');
 INSERT INTO `reader_info` VALUES (1706010222, '小刚', '男', '1999-05-03', '山东省青岛市', '15369874123');
+INSERT INTO `reader_info` VALUES (1706010300, 'ganzong', '男', '2023-12-10', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (1706010220, 'ganzong', '男', '2023-12-10', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (1706010240, 'aganaa', '男', '2023-12-30', '广东', '13413811594');
+INSERT INTO `reader_info` VALUES (2006010217, 'qq', '男', '2023-12-31', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (2110005288, 'aa', '男', '1998-09-10', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (0, 'agan', '男', '2023-12-11', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (0, 'agan', '男', '2023-12-11', '广东', '13413811599');
+INSERT INTO `reader_info` VALUES (0, 'xf', '男', '2023-12-17', '广东', '13413811594');
+INSERT INTO `reader_info` VALUES (0, 'wxf', '男', '2023-12-24', '广东', '12345678912');
+INSERT INTO `reader_info` VALUES (170601000, 'qy', '男', '2023-12-23', '广东', '13413811594');
+INSERT INTO `reader_info` VALUES (115155, 'aganaa', '男', '2023-12-28', '广东', '13413811599');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-  `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限',
+  `username` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '密码',
+  `role` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '权限',
   PRIMARY KEY (`username`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('0918', '123', 'user');
+INSERT INTO `user` VALUES ('1006010217', '123', 'user');
+INSERT INTO `user` VALUES ('115155', '123', 'user');
+INSERT INTO `user` VALUES ('170601000', '123', 'user');
 INSERT INTO `user` VALUES ('1706010217', '111111', 'user');
 INSERT INTO `user` VALUES ('1706010218', '111111', 'user');
 INSERT INTO `user` VALUES ('1706010219', '123456', 'user');
 INSERT INTO `user` VALUES ('1706010220', '111111', 'user');
 INSERT INTO `user` VALUES ('1706010221', '111111', 'user');
 INSERT INTO `user` VALUES ('1706010222', '111111', 'user');
-INSERT INTO `user` VALUES ('admin', '111111', 'admin');
+INSERT INTO `user` VALUES ('2110005288', '123456', 'user');
+INSERT INTO `user` VALUES ('admin', '1234567', 'admin');
+INSERT INTO `user` VALUES ('gan', '123456', 'user');
 
 SET FOREIGN_KEY_CHECKS = 1;
